679a55034661dff4a438085ed4a7c0f111b2fd13
Paul <easylive1989@gmail.com>
2023-02-19 15:07:52 +0800

    add ball

--- lib/break_out_game/breakout_game.dart
+++ lib/break_out_game/breakout_game.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_breakout/break_out_game/brick_generator.dart';
 import 'package:flutter_breakout/break_out_game/paddle.dart';
 
@@ -12,5 +14,10 @@ class BreakoutGame extends FlameGame with HasKeyboardHandlerComponents {
 
     add(Paddle(size));
     add(BrickGenerator(position: Vector2(size.x / 2, size.y * 0.3)));
+    add(CircleComponent(
+      radius: 10,
+      position: size / 2..add(Vector2(-5, size.y * 0.3)),
+      paint: Paint()..color = Colors.white,
+    ));
   }
 }
