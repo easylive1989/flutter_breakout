6550a91c1a9f74097cf6dd91a72a8727470d83ce
Paul <easylive1989@gmail.com>
2023-02-19 15:00:35 +0800

    add color for brick

--- lib/break_out_game/brick_generator.dart
+++ lib/break_out_game/brick_generator.dart
@@ -1,10 +1,26 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_breakout/break_out_game/breakout_game.dart';
 
 class BrickGenerator extends PositionComponent with HasGameRef<BreakoutGame> {
   BrickGenerator({super.position}) : super(anchor: Anchor.center);
+
+  final List<Color> colors = [
+    Colors.white,
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+    Colors.grey,
+    Colors.black,
+    Colors.brown,
+  ];
 
   @override
   FutureOr<void> onLoad() {
@@ -17,12 +33,14 @@ class BrickGenerator extends PositionComponent with HasGameRef<BreakoutGame> {
 
     List.generate(totalYofBricks, (yIndex) {
       List.generate(totalXofBricks, (xIndex) {
+        var paint = Paint()..color = colors[(xIndex + yIndex) % colors.length];
         add(RectangleComponent(
-          position: Vector2(
-            initXofBricks + xIndex * 50,
-            initYofBricks + yIndex * 20,
-          ),
-          size: Vector2(50, 20),
+            position: Vector2(
+              initXofBricks + xIndex * 50,
+              initYofBricks + yIndex * 20,
+            ),
+            size: Vector2(50, 20),
+            paint: paint,
         ));
       });
     });
